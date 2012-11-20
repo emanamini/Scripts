d222eaf5a5f947e0cc76877e3ae3d04980fb8fd7
ishahinism@gmail.com <ishahinism@gmail.com>
2012-11-20 10:48:09 +0330

    Now I will check for every item in an element and then generate the output

--- Python/kvtml2anki/kvtml2anki.py
+++ Python/kvtml2anki/kvtml2anki.py
@@ -10,28 +10,32 @@ except:
 def main(file_name):
     tree = ET.ElementTree(file=file_name)
 
-    up = [] # List of objects to place on top of the card
-    down = [] # List of objects to place on buttom of the card
+#    up = [] # List of objects to place on top of the card
+#    down = [] # List of objects to place on buttom of the card
 
+    up = ""
+    down = ""
+    
     # translation with id 0 is top of the card and with id 1 is buttom of it.
     for elem in tree.iterfind('entries/entry/translation'):
-        # to export all sub entry in translations I used astrik ;-)
-        for item in elem.iterfind('*'):
+        # In the previous version I used an astrik sign to ge all
+        # items in a element. But if there was an element without
+        # sound or image that solution would generate brokened out
+        # put. In this solution I'll check every item and if they
+        # exists then I'll add them to the output string.
+        for item in elem.iterfind('text'):
             if elem.attrib['id'] == '0':
-                up.append(item.text)
+                up = item.text
             else:
-                down.append(item.text)
-
-    output = '' # Final generated string. it'll print on stdout.
-
-    # Loop on generated list (up, down) every two step:
-    for i in range(0, len(up), 2):
-        # cut / from the media paths. it's lasy style ;-)
-        sound = up[i+1].find('/')
-        image = down[i+1].find('/')
-        output += up[i] + "[sound:" + up[i+1][sound+1:] + "];<img src='" + down[i+1][image+1:] + "'/><br />" + down[i].encode('utf-8') + "\n"
-        print output
-
+                down = item.text
+        if elem.iterfind('sound'):
+            for item in elem.iterfind('sound'):
+                up = up + "[sound:" + item.text +"]"
+        if elem.iterfind('image'):
+            for item in elem.iterfind('image'):
+                down = "<img src='" + item.text + "'/><br />" + down
+        print up + ";" + down
+                
 if __name__ == '__main__':
     # check for cmmand line arguments
     if len(sys.argv) < 2:
